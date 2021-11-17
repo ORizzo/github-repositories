b863393cb92389a07a5a53a5035f59ee096bb324
ORizzo <henriquerizzomor@gmail.com>
2021-11-17 20:24:38 -0300

    commit inicial

---
+++ git-repos/pages/repos.js
@@ -0,0 +1,23 @@
+import useSWR from "swr";
+import fetch from "unfetch";
+
+export default function repos() {
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+  const { data, error } = useSWR(
+    "https://api.github.com/users/ORizzo/repos",
+    fetcher
+  );
+  if (error) return <div>Failed to load</div>; // continuar estilizando os cards com os repos e continuar o desenvolvimento
+  if (!data) return <div>Loading...</div>;
+  const repos = data.map((repo) => 
+  <div>
+      <h2 className="bg-gray-400">{repo.name}</h2>
+      <p></p>
+  </div>
+  );
+  return(
+    <>
+    {repos}
+    </>
+  );
+}
