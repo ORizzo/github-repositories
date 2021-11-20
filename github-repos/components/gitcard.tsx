e9e3163c1c4201e1a3277130d62eb4f70f37334b
ORizzo <henriquerizzomor@gmail.com>
2021-11-20 16:58:12 -0300

    adição de toasts e geração automática dos cards dos repos

--- github-repos/components/gitcard.tsx
+++ github-repos/components/gitcard.tsx
@@ -1,15 +1,31 @@
-import { Image, Box, Icon } from "@chakra-ui/react";
-import { BsGithub } from 'react-icons/bs'
-export default function GitCard(): JSX.Element {
+import { Box, Icon, Badge, Button, useToast } from "@chakra-ui/react";
+import { BsGithub } from "react-icons/bs";
+import { useClipboard } from "@chakra-ui/hooks";
+import { MdContentCopy } from "react-icons/md";
+export default function GitCard(props): JSX.Element {
+  const repo = props.info;
+  const { hasCopied, onCopy } = useClipboard(`git clone ${repo.clone_url}`);
+  const toast = useToast()
+  function CloningRepo(){
+    onCopy()
+    toast({
+      title: "Repositório clonado.",
+      description: "O link para clonar o repositório foi copiado.",
+      status: "success",
+      duration: 4000,
+      isClosable: true,
+    })
+  }
   return (
     <Box
-      maxW="sm"
+      maxW="lg"
       borderWidth="2px"
-      borderColor="blue"
+      borderColor="purple.400"
       background="gray.800"
       borderRadius="lg"
       overflow="hidden"
-      m={10}>
+      m={10}
+      shadow="xl">
       <Box p="4">
         <Box
           mx="2"
@@ -19,11 +35,24 @@ export default function GitCard(): JSX.Element {
           fontSize="3xl"
           lineHeight="tight"
           isTruncated>
-          <Icon as={BsGithub} mr="3"></Icon>{"Nome do repo"}
+          <Icon as={BsGithub} mr="3"></Icon>
+          {repo.name}
+          <Badge mx="3" bgColor="purple.600" borderRadius="sm">
+            {repo.language}
+          </Badge>
         </Box>
 
         <Box mx="2" my="1" as="h4" fontSize="lg" lineHeight="tight" isTruncated>
-          {"Descrição do repo"}
+          <Button
+            onClick={CloningRepo}
+            size="md"
+            variant="outline"
+            leftIcon={<MdContentCopy />}
+            _focus={{
+              borderColor: "gray.600",
+            }}>
+            {hasCopied ? "Clonado" : "Clonar"}
+          </Button>
         </Box>
       </Box>
     </Box>
--- github-repos/pages/repos.tsx
+++ github-repos/pages/repos.tsx
@@ -1,5 +1,19 @@
 import GitCard from "../components/gitcard";
+import useSWR from "swr";
+import axios from "axios";
+import { SimpleGrid } from "@chakra-ui/layout";
 
 export default function GitRepos() {
-  return <GitCard></GitCard>;
+  const fetcher = (url) => axios.get(url).then((res) => res.data);
+  const { data, error } = useSWR(
+    "https://api.github.com/users/ORizzo/repos",
+    fetcher
+  );
+
+  if (error) return <div>{error}</div>;
+  if (!data) return <div>loading...</div>;
+  const repos = data.map((repo) => (
+    <GitCard key={repo.id} info={repo}></GitCard>
+  ));
+  return <SimpleGrid columns={2}>{repos}</SimpleGrid>;
 }
