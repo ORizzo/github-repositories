2b3763cb0d456a4f818a5448317cf861bcca19a1
ORizzo <henriquerizzomor@gmail.com>
2021-11-26 18:24:55 -0300

    mudança s na estrutra, agora usando getstaticprops

--- github-repos/pages/index.tsx
+++ github-repos/pages/index.tsx
@@ -1,30 +1,21 @@
 import GitCard from "../components/gitcard";
-import useSWR from "swr";
 import axios from "axios";
 import { SimpleGrid } from "@chakra-ui/layout";
-import Loading from "../components/loading";
 
-export default function GitRepos() {
-  const fetcher = (url) => axios.get(url).then((res) => res.data);
-  const { data, error } = useSWR(
-    "https://api.github.com/users/ORizzo/repos",
-    fetcher
-  );
-
-  if (error) return <div>{error}</div>;
-  if (!data)
-    return (
-      <SimpleGrid columns={{ sm: 1, md: 2 }}>
-        <Loading></Loading>
-        <Loading></Loading>
-        <Loading></Loading>
-        <Loading></Loading>
-        <Loading></Loading>
-        <Loading></Loading>
-      </SimpleGrid>
-    );
+export default function Index({ data }) {
   const repos = data.map((repo) => (
     <GitCard key={repo.id} info={repo}></GitCard>
   ));
   return <SimpleGrid columns={{ sm: 1, md: 2 }}>{repos}</SimpleGrid>;
+}
+
+export async function getStaticProps() {
+  const { data } = await axios.get("https://api.github.com/users/ORizzo/repos");
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 60, // In seconds
+  };
 }
